8e972129b804a3c61bc6c0751664baff0351ad18
Maciej Stawarz <maciey138@gmail.com>
2023-10-19 21:40:26 +0200

    add comments and docstrings (AI generated)

--- tasklist.py
+++ tasklist.py
@@ -2,31 +2,41 @@ import tkinter as tk
 from window import Win
 from tkinter import filedialog
 
-# colors
+# Constants for colors
 BLACK = 'black'
 DARK_GRAY = '#1f1f1f'
 WHITE = 'white'
 
-
 class TaskListApp:
+    """Task list application."""
+
     def __init__(self, root: Win):
+        """Initialize the task list application.
+
+        Args:
+            root (Win): The main application window.
+
+        """
         self.root = root
         self.root.title("")
         self.tasks = []
         self.create_widgets()
 
     def create_widgets(self):
-        # create necessary widgets
+        """Create application widgets and set up event bindings."""
+
+        # Create an entry widget for task input
         self.entry = tk.Entry(self.root, bg=DARK_GRAY, fg=WHITE,
                               justify='left', highlightbackground=DARK_GRAY, relief='solid')
         self.entry.pack(fill='x')
         self.entry.bind('<Return>', self.add_task)
 
+        # Create a listbox for displaying tasks
         self.task_list = tk.Listbox(self.root, bg=DARK_GRAY, fg=WHITE, selectbackground=DARK_GRAY,
                                     highlightbackground=DARK_GRAY, relief='solid', activestyle='none')
         self.task_list.pack(expand=True, fill='both')
 
-        # create bindings for task actions
+        # Create bindings for task actions
         self.task_list.bind('<Double-Button-1>', self.delete_task)
         self.task_list.bind('<ButtonRelease-1>', self.toggle_done)
 
@@ -42,28 +52,40 @@ class TaskListApp:
             label="Exit", command=self.root.close)
         self.task_list.bind("<Button-3>", self.show_context_menu)
 
-        # focus on entry widget so that input is immediately possible
+        # Focus on entry widget so that input is immediately possible
         self.entry.focus_set()
 
     def add_task(self, _):
-        # get task text
+        """Add a task to the list of tasks.
+
+        Args:
+            _: Unused event argument.
+
+        """
+        # Get task text
         task_text = self.entry.get()
 
         if task_text:
-            # add task at the start of the internal list
+            # Add task at the start of the internal list
             self.tasks.insert(0, task_text)
 
-            # add task at the top of the listbox
+            # Add task at the top of the listbox
             self.task_list.insert(0, task_text)
 
-            # clear the entry widget
+            # Clear the entry widget
             self.entry.delete(0, 'end')
 
     def toggle_done(self, event):
+        """Toggle the done status of a task.
+
+        Args:
+            event: Mouse event containing click information.
+
+        """
         y = event.y
         nearest_item_index = self.task_list.nearest(y)
 
-        # Listbox.nearest() returns -1 if empty of items
+        # Listbox.nearest() returns -1 if the list is empty
         if nearest_item_index > -1:
             task_geometry = self.task_list.bbox(nearest_item_index)
             if task_geometry[2] and is_event_within_bbox(event, task_geometry, ignore_x=True):
@@ -77,10 +99,16 @@ class TaskListApp:
                     self.task_list.insert(nearest_item_index,
                                           self.tasks[nearest_item_index])
 
-        # Set focus to the entry widget even if no task was toggled (handles dragging of empty tasklist)
+        # Set focus to the entry widget even if no task was toggled (handles dragging of empty task list)
         self.entry.focus_set()
 
     def delete_task(self, event):
+        """Delete a task from the list.
+
+        Args:
+            event: Mouse event containing click information.
+
+        """
         selected_index = self.task_list.nearest(event.y)
         if selected_index >= 0:
             # Disable toggle event handling
@@ -94,13 +122,21 @@ class TaskListApp:
             self.root.after(100, self.enable_event_handling)
 
     def enable_event_handling(self):
+        """Re-enable event handling after a short delay."""
         # Re-enable event handling after a short delay
         self.task_list.bind('<ButtonRelease-1>', self.toggle_done)
 
     def show_context_menu(self, event):
+        """Display the context menu.
+
+        Args:
+            event: Mouse event containing the click position.
+
+        """
         self.context_menu.post(event.x_root, event.y_root)
 
     def save_tasks(self):
+        """Save tasks to a text file."""
         # Ask the user to select a file to save the tasks
         file_path = filedialog.asksaveasfilename(
             defaultextension=".txt", filetypes=[("Text Files", "*.txt")])
@@ -114,6 +150,7 @@ class TaskListApp:
                 print(f"Error saving tasks: {e}")
 
     def load_tasks(self):
+        """Load tasks from a text file."""
         # Ask the user to select a file to load tasks from
         file_path = filedialog.askopenfilename(
             filetypes=[("Text Files", "*.txt")])
@@ -128,8 +165,18 @@ class TaskListApp:
             except Exception as e:
                 print(f"Error loading tasks: {e}")
 
-
 def is_event_within_bbox(event, bbox, ignore_x=False):
+    """Check if the event is within a given bounding box.
+
+    Args:
+        event: Mouse event containing the click position.
+        bbox: Bounding box (x, y, width, height).
+        ignore_x: Flag to ignore the x-coordinate.
+
+    Returns:
+        bool: True if the event is within the bounding box, False otherwise.
+
+    """
     x, y, width, height = int(bbox[0]), int(
         bbox[1]), int(bbox[2]), int(bbox[3])
 
@@ -140,19 +187,18 @@ def is_event_within_bbox(event, bbox, ignore_x=False):
         # Check if the event's x and y coordinates are within the bounding box
         return x <= event.x <= x + width and y <= event.y <= y + height
 
-
 if __name__ == "__main__":
-    # prepare the app
+    # Prepare the app
     root = Win()
     root.geometry("180x240")
     root.configure(bg="black")
     app = TaskListApp(root)
 
-    # keep the app always in the foreground
+    # Keep the app always in the foreground
     root.attributes('-topmost', 1)
 
-    # # disallow resizing of the window
+    # # Disallow resizing of the window
     # root.resizable(False, False)
 
-    # run the app
+    # Run the app
     root.mainloop()
--- window.py
+++ window.py
@@ -1,43 +1,81 @@
 import tkinter as tk
 import sys
 
-
 class Win(tk.Tk):
+    """Custom window class for the task list application."""
+
     click_pos = (0, 0)
 
     def __init__(self):
-        super().__init__()
+        """Initialize the window with specific settings and event bindings.
 
-        # disable window manager on the app's window
+        This constructor sets up the window without window decorations, places it
+        in the bottom right part of the screen, and binds mouse and keyboard events.
+
+        """
+        super().__init()
+
+        # Disable window manager on the app's window
         super().overrideredirect(True)
 
-        # start the window in the bottom right part of the screen
+        # Start the window in the bottom right part of the screen
         self._offsetx = int(0.8 * self.winfo_screenwidth())
         self._offsety = int(0.8 * self.winfo_screenheight())
         super().geometry(f"+{self._offsetx}+{self._offsety}")
 
-        # bind mouse events
+        # Bind mouse events
         super().bind("<Button-1>", self.clickwin)
         super().bind("<B1-Motion>", self.dragwin)
 
-        # bind closing key
+        # Bind closing key
         super().bind("<Escape>", self.close)
 
     def dragwin(self, _):
-        # move the window with the cursor
+        """Handle window dragging based on cursor movement.
+
+        This function moves the window with the cursor as it's dragged.
+
+        Args:
+            _: Unused event argument.
+
+        """
         x = super().winfo_pointerx() - self._offsetx
         y = super().winfo_pointery() - self._offsety
         super().geometry(f"+{x}+{y}")
 
     def clickwin(self, event):
-        # Track the click position on the general screen
+        """Handle mouse click and track the click position on the screen.
+
+        This function records the position of a mouse click on the screen and the window's
+        offset to the cursor.
+
+        Args:
+            event: Mouse click event.
+
+        """
         self.click_pos = (event.x_root, event.y_root)
         self._offsetx = super().winfo_pointerx() - super().winfo_rootx()
         self._offsety = super().winfo_pointery() - super().winfo_rooty()
 
     def has_cursor_moved(self):
-        # Compare the cursor positions at press and release
+        """Check if the cursor has moved since the last click.
+
+        This function compares the current cursor position with the position of the last click
+        to determine if the cursor has moved.
+
+        Returns:
+            bool: True if the cursor has moved, False otherwise.
+
+        """
         return self.click_pos != (super().winfo_pointerx(), super().winfo_pointery())
 
     def close(self, _=None):
+        """Exit the application.
+
+        This function closes the application.
+
+        Args:
+            _: Unused event argument.
+
+        """
         sys.exit()
